adacaee19b1ff062f5b690a7d883164e8d237285
Guillaume Theaud <guillaume.theaud@gmail.com>
2019-11-01 10:37:21 -0400

    Fix setup

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ opts = dict(name=NAME,
             install_requires=REQUIRES,
             requires=REQUIRES,
             scripts=SCRIPTS,
-            package_data={"dmriqcpy": ["template/*", "template/libs/*"]})
+            data_files=[("template", "*")])
 
 
 if __name__ == '__main__':
