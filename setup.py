7303710e7601ca4a8b87777631327cd445ef46a0
Guillaume Theaud <guillaume.theaud@gmail.com>
2019-11-01 10:29:35 -0400

    Fix setup

--- setup.py
+++ setup.py
@@ -25,7 +25,8 @@ opts = dict(name=NAME,
             packages=PACKAGES,
             install_requires=REQUIRES,
             requires=REQUIRES,
-            scripts=SCRIPTS)
+            scripts=SCRIPTS,
+            package_data={"": ["template"]})
 
 
 if __name__ == '__main__':
