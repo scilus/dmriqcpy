d254940c6068b0de850b68a0ae0ab9c0a206b43e
arnaudbore <arnaudbore@gmail.com>
2022-03-11 11:52:43 -0500

    add possibility to compute lut

--- scripts/dmriqc_labels.py
+++ scripts/dmriqc_labels.py
@@ -28,16 +28,13 @@ def _build_arg_parser():
                                 formatter_class=argparse.RawTextHelpFormatter)
 
     p.add_argument('output_report',
-                   help='HTML report')
-
-    p.add_argument('lut',
-                   help='Look Up Table for RGB')
+                   help='HTML report.')
 
     p.add_argument('--t1', nargs='+', required=True,
-                   help='T1 images in Nifti format')
+                   help='T1 images in Nifti format.')
 
     p.add_argument('--label', nargs='+', required=True,
-                   help='Label images in Nifti format')
+                   help='Label images in Nifti format.')
 
     p.add_argument('--skip', default=2, type=int,
                    help='Number of images skipped to build the '
@@ -45,6 +42,12 @@ def _build_arg_parser():
 
     p.add_argument('--nb_columns', default=12, type=int,
                    help='Number of columns for the mosaic. [%(default)s]')
+
+    p.add_argument('--lut', nargs=1, default="",
+                   help='Look Up Table for RGB.')
+
+    p.add_argument('--compute_lut', action='store_true',
+                   help='Compute Look Up Table for RGB.')
 
     p.add_argument('--nb_threads', type=int, default=1,
                    help='Number of threads. [%(default)s]')
@@ -55,17 +58,23 @@ def _build_arg_parser():
     return p
 
 
-def _subj_parralel(t1, label, name, skip, nb_columns, lut):
+def _subj_parralel(t1, label, name, skip, nb_columns, lut, compute_lut):
     subjects_dict = {}
+    if not lut:
+        lut = None
+
     screenshot_path = screenshot_mosaic_blend(t1, label,
                                               output_prefix=name,
                                               directory="data",
-                                              blend_val=0.5,
+                                              blend_val=0.4,
                                               skip=skip, nb_columns=nb_columns,
-                                              lut=lut)
+                                              lut=lut,
+                                              compute_lut=compute_lut)
 
-    subjects_dict[t1] = {}
-    subjects_dict[t1]['screenshot'] = screenshot_path
+    key = os.path.basename(t1)
+
+    subjects_dict[key] = {}
+    subjects_dict[key]['screenshot'] = screenshot_path
     return subjects_dict
 
 
@@ -76,7 +85,10 @@ def main():
     if not len(args.t1) == len(args.label):
         parser.error("Not the same number of images in input.")
 
-    all_images = np.concatenate([args.t1, args.label, [args.lut]])
+    all_images = np.concatenate([args.t1, args.label])
+    if args.lut:
+        all_images = np.concatenate(all_images, args.lut)
+
     assert_inputs_exist(parser, all_images)
     assert_outputs_exist(parser, args, [args.output_report, "data", "libs"])
 
@@ -96,7 +108,8 @@ def main():
                                           itertools.repeat(name),
                                           itertools.repeat(args.skip),
                                           itertools.repeat(args.nb_columns),
-                                          itertools.repeat(args.lut)))
+                                          itertools.repeat(args.lut),
+                                          itertools.repeat(args.compute_lut)))
     pool.close()
     pool.join()
 
